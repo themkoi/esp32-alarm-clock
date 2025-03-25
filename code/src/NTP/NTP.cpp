ea483ebca3cb0c24507f91fc71e41156172cd729
KitKat-ok <Pascalsch3@gmail.com>
2025-03-25 17:35:06 +0100

    timezone

--- code/src/NTP/NTP.cpp
+++ code/src/NTP/NTP.cpp
@@ -1,48 +1,28 @@
 #include "NTP.h"
+#include <Timezone.h>
 
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP);
 TaskHandle_t NTPTask;
 
-// Define your timezone and DST rules
-const int TIME_ZONE = 1; // Poland is UTC+1
-bool isDST = false;      // Initialize DST status
-
-// Function to check if DST is in effect for the current date
-bool isDaylightSavingTime()
-{
-  // Get the current year, month, and day
-  int currentYear = year();
-  int currentMonth = month();
-  int currentDay = day();
-
-  // Calculate the last Sunday in March
-  int lastSundayMarch = 31 - (5 + (currentYear * 5 / 4 + 4) % 7);
-
-  // Calculate the last Sunday in October
-  int lastSundayOctober = 31 - (5 + (currentYear * 5 / 4 + 2) % 7);
-
-  // Check if the current date falls within the DST period
-  if ((currentMonth > 3 || (currentMonth == 3 && currentDay >= lastSundayMarch)) &&
-      (currentMonth < 10 || (currentMonth == 10 && currentDay < lastSundayOctober)))
-  {
-    return true;
-  }
-  return false;
-}
+// Define Timezone rules for Poland (Central European Time)
+TimeChangeRule myDST = {"CEST", Last, Sun, Mar, 2, 120}; // UTC+2
+TimeChangeRule mySTD = {"CET", Last, Sun, Oct, 3, 60};  // UTC+1
+Timezone myTZ(myDST, mySTD);
+TimeChangeRule *tcr;
 
 void syncTimeTask(void *parameter);
 
 void createTimeTask()
 {
   xTaskCreatePinnedToCore(
-      syncTimeTask,   // Function to implement the task
-      "SyncTimeTask", // Name of the task
-      10000,          // Stack size (words)
-      NULL,           // Parameter to pass
-      1,              // Priority
-      &NTPTask,       // Task handle
-      1               // Core to run the task on (Core 1)
+      syncTimeTask,   
+      "SyncTimeTask", 
+      10000,          
+      NULL,           
+      1,              
+      &NTPTask,       
+      1               
   );
 }
 
@@ -52,30 +32,19 @@ void deleteTimeTask()
 }
 
 void syncTimeLibWithRTC() {
-  // Prepare the timeval structure
   struct timeval tv;
-  gettimeofday(&tv, NULL);  // Read the current time from the internal RTC
-
-  // Set the time in TimeLib
+  gettimeofday(&tv, NULL);
   setTime(tv.tv_sec);
-
   Serial.println("TimeLib synchronized with internal RTC");
 }
 
-
 void syncESP32RTC()
 {
-  // Get the current time from TimeLib
   time_t now = ::now();
-
-  // Prepare the timeval structure
   struct timeval tv;
   tv.tv_sec = now;
   tv.tv_usec = 0;
-
-  // Set the time to the internal RTC
   settimeofday(&tv, NULL);
-
   Serial.println("Internal RTC synchronized with TimeLib");
 }
 
@@ -83,19 +52,14 @@ void syncTimeTask(void *parameter)
 {
   while (true)
   {
-    // Check if WiFi is connected
     if (WiFi.status() == WL_CONNECTED)
     {
       synchronizeAndSetTime();
-
     }
     else
     {
-      // WiFi is not connected, print a message or take appropriate action
       Serial.println("WiFi not connected. Cannot sync time.");
     }
-
-    // Wait for 10 minutes before the next synchronization
     vTaskDelay(pdMS_TO_TICKS(10 * 60 * 1000));
   }
 }
@@ -105,29 +69,12 @@ void synchronizeAndSetTime()
   Serial.println("Synchronizing Time");
   timeClient.begin();
   timeClient.update();
-
-  // Check if DST is in effect
-  if (isDaylightSavingTime())
-  {
-    isDST = true;
-  }
-  else
-  {
-    isDST = false;
-  }
-
-  // Adjust time offset for DST
-  if (isDST)
-  {
-    timeClient.setTimeOffset(TIME_OFFSET_S + 3600); // Add one hour for DST
-  }
-  else
-  {
-    timeClient.setTimeOffset(TIME_OFFSET_S);
-  }
-
-  setTime(timeClient.getEpochTime()); // Set the system time
+  
+  time_t utc = timeClient.getEpochTime();
+  time_t local = myTZ.toLocal(utc, &tcr);
+  
+  setTime(local);
   timeClient.end();
   syncESP32RTC();
-  Serial.println("Current time " + String(hour()) + ":" + String(minute()));
+  Serial.println("Current time: " + String(hour()) + ":" + String(minute()) + " " + tcr->abbrev);
 }
